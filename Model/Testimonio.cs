f8eb41e9edc3bc39fdfdca5a546bdc57e10031c8
Daniel <Daniel@DESKTOP-MIILPDK>
2018-01-25 03:34:05 +0100

    inclusión de mantenimiento de testimonios

--- Model/Testimonio.cs
+++ Model/Testimonio.cs
@@ -4,7 +4,9 @@ namespace Model
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("Testimonio")]
     public partial class Testimonio
@@ -30,5 +32,121 @@ namespace Model
         public string Fecha { get; set; }
 
         public virtual Usuario Usuario { get; set; }
+
+        public Testimonio Obtener(int id)
+        {
+            var testimonio = new Testimonio();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    testimonio = ctx.Testimonio.Where(x => x.id == id)
+                                                 .SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return testimonio;
+        }
+
+        public ResponseModel Guardar()
+        {
+            var rm = new ResponseModel();
+
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    if (this.id > 0) ctx.Entry(this).State = EntityState.Modified;
+                    else ctx.Entry(this).State = EntityState.Added;
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return rm;
+        }
+
+        public ResponseModel Eliminar(int id)
+        {
+            var rm = new ResponseModel();
+
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    this.id = id;
+                    ctx.Entry(this).State = EntityState.Deleted;
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return rm;
+        }
+
+        public AnexGRIDResponde Listar(AnexGRID grid, int Usuario_id)
+        {
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    grid.Inicializar();
+                    var query = ctx.Testimonio.Where(x => x.Usuario_id == Usuario_id);
+
+                    // Ordenamiento
+                    if (grid.columna == "id")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.id)
+                                                             : query.OrderBy(x => x.id);
+                    }
+
+                    if (grid.columna == "Nombre")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Nombre)
+                                                             : query.OrderBy(x => x.Nombre);
+                    }
+
+                    if (grid.columna == "IP")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.IP)
+                                                             : query.OrderBy(x => x.IP);
+                    }
+
+                    if (grid.columna == "Fecha")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Fecha)
+                                                             : query.OrderBy(x => x.Fecha);
+                    }
+
+                    // id, Nombre, Titulo, Desde, Hasta
+
+                    var experiencias = query.Skip(grid.pagina)
+                                            .Take(grid.limite)
+                                             .ToList();
+
+                    var total = query.Count();
+
+                    grid.SetData(experiencias, total);
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+
+            return grid.responde();
+        }
     }
 }
--- proyecto/Areas/Admin/Controllers/TestimoniosController.cs
+++ proyecto/Areas/Admin/Controllers/TestimoniosController.cs
@@ -1,4 +1,6 @@
-﻿using proyecto.Areas.Admin.Filters;
+﻿using Helper;
+using Model;
+using proyecto.Areas.Admin.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,36 @@ namespace proyecto.Areas.Admin.Controllers
     [Autenticado]
     public class TestimoniosController : Controller
     {
+        private Testimonio testimonio = new Testimonio();
         // GET: Admin/Testimonios
         public ActionResult Index()
         {
             return View();
         }
+        public JsonResult Listar(AnexGRID grid) => Json(testimonio.Listar(grid, SessionHelper.GetUser()));
+
+        public ActionResult Crud(int id)
+        {
+            return View(testimonio.Obtener(id));
+        }
+
+        public JsonResult Guardar(Testimonio model)
+        {
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                rm = model.Guardar();
+
+                if (rm.response)
+                {
+                    rm.href = Url.Content("~/admin/testimonios");
+                }
+            }
+
+            return Json(rm);
+        }
+
+        public JsonResult Eliminar(int id) => Json(testimonio.Eliminar(id));
     }
 }
