ceb66a53db6ba80329f07d5cc2b1cf655bc21725
Daniel <Daniel@DESKTOP-MIILPDK>
2018-04-19 06:05:39 +0200

    Uso de meta data en clases modelo

---
+++ Model/HabilidadMetaData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [MetadataType(typeof(HabilidadMetaData))]
+    public partial class Habilidad
+    {
+        class HabilidadMetaData
+        {
+            [Display(Name = "Nombre de habilidad" )]
+            public object Nombre { get; set; }
+        }
+    }
+}
